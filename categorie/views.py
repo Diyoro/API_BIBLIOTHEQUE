d5821ab093080a5a7ccc618e95f916c2f5ed43a8
Diyoro <106103898+Diyoro@users.noreply.github.com>
2023-08-10 18:43:14 +0000

    Deuxième dépot

--- categorie/views.py
+++ categorie/views.py
@@ -6,8 +6,15 @@ from django.http import JsonResponse
 
 
 def Biblio(request):
+    ip_address = '192.168.1.73'
+    port = '8000'
+    livres = list(Livre.objects.all().values())
+
+    for livre in livres:
+        livre['document'] = f'http://{ip_address}:{port}/api/v1/books/'
+        livre['image'] = f'http://{ip_address}:{port}/api/v1/books/'
     context = {   
-    "livres" : list(Livre.objects.all().values()),
+    "livres" : livres,
     }
     
     return JsonResponse(context['livres'], safe=False)
